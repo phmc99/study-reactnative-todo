0dc45c9d8b8f5e3a2157d016b78c6510652fbaf9
Pedro Costa <phmc99@outlook.com>
2025-03-04 00:53:48 -0300

    WIP - creating input for todos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { homeStyles } from './style';
 
 export default function Home() {
+  const handleAddTodo = () => {
+    console.log("Check")
+  }
+
   return (
     <View style={homeStyles.container}>
-      <Text>Home</Text>
-      <StatusBar style="auto" />
+      <Text>To Do</Text>
+      <View style={homeStyles.flex}>
+        <TextInput style={homeStyles.input} placeholder='What you want to do?' />
+        <TouchableOpacity style={homeStyles.createButton} onPress={handleAddTodo}><Text style={homeStyles.createButtonText}>+</Text></TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -8,4 +8,32 @@ export const homeStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flex: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 2,
+    width: "100%"
+  },
+  input: {
+     width: "80%",
+     height: 56,
+     backgroundColor: "#ccc",
+     padding: 16,
+     fontSize: 16,
+     borderRadius: 10
+  },
+  createButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#4ea8de",
+    borderRadius: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  createButtonText: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold"
+  }
 });
